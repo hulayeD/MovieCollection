a528450fd8beb303f1930afa61982990d60f6b05
BT_1E10_28 <jeffreyc115@nycstudents.net>
2022-03-01 09:51:18 -0500

    ok

---
+++ src/MovieCollectionRunner.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class MovieCollectionRunner
+{
+  public static void main(String[] args)
+  {
+   MovieCollection collection = new MovieCollection("src\\movie.csv");
+    ArrayList<Movie> movieList = collection.getMovies();
+    for (Movie movie : movieList)
+    {
+      System.out.println(movie);
+    }
+
+
+  }
+}
